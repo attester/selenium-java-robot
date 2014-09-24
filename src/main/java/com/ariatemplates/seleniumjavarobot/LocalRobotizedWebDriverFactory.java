attester/selenium-java-robot

commit 5f2462db6e57cf3a5ab0d3fc2c477acb1eb1d0db

Big refactoring and adding support for closing the browser on process exit.

src/main/java/com/ariatemplates/seleniumjavarobot/LocalRobotizedWebDriverFactory.java
@@ -1,0 +1,129 @@
+/*
+ * Copyright 2014 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ariatemplates.seleniumjavarobot;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.lang.reflect.Constructor;
+
+import org.apache.commons.exec.OS;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.internal.ProfilesIni;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public abstract class LocalRobotizedWebDriverFactory implements IRobotizedWebDriverFactory {
+
+    private IRobot robot;
+
+    private static void numLockStateWorkaround() {
+        try {
+            // Shift key is not kept pressed while using keyPress method
+            // cf https://forums.oracle.com/thread/2232592
+            // The workaround is to use the following line:
+            Toolkit.getDefaultToolkit().setLockingKeyState(KeyEvent.VK_NUM_LOCK, false);
+            SeleniumJavaRobot.log("Num lock state was successfully changed.");
+        } catch (UnsupportedOperationException e) {
+            SeleniumJavaRobot.log("Did not change num lock state: " + e);
+        }
+    }
+
+    public LocalRobotizedWebDriverFactory() {
+        try {
+            robot = new LocalRobot(new Robot());
+        } catch (AWTException e) {
+            throw new RuntimeException(e);
+        }
+        numLockStateWorkaround();
+    }
+
+    public abstract RemoteWebDriver createWebDriver();
+
+    public RobotizedWebDriver createRobotizedWebDriver() {
+        RemoteWebDriver driver = createWebDriver();
+        if (OS.isFamilyMac()) {
+            try {
+                // put the browser in the foreground:
+                Runtime.getRuntime().exec("open -a \"" + driver.getCapabilities().getBrowserName() + "\"");
+            } catch (Exception e) {
+            }
+        }
+        driver.manage().window().maximize();
+        return new RobotizedWebDriver(robot, driver);
+    }
+
+    public static class LocalFirefox extends LocalRobotizedWebDriverFactory {
+        private final FirefoxProfile firefoxProfile;
+
+        public LocalFirefox(FirefoxProfile firefoxProfile) {
+            this.firefoxProfile = firefoxProfile;
+        }
+
+        @Override
+        public RemoteWebDriver createWebDriver() {
+            return new FirefoxDriver(firefoxProfile);
+        }
+    }
+
+    public static class LocalBrowser<T extends RemoteWebDriver> extends LocalRobotizedWebDriverFactory {
+        private final Constructor<T> webdriverClass;
+
+        public LocalBrowser(Class<T> webdriverClass) {
+            try {
+                this.webdriverClass = webdriverClass.getConstructor();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        public RemoteWebDriver createWebDriver() {
+            try {
+                return webdriverClass.newInstance();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static LocalRobotizedWebDriverFactory createRobotizedWebDriverFactory(String browser) {
+        if (BrowserType.FIREFOX.equalsIgnoreCase(browser)) {
+            FirefoxProfile firefoxProfile = null;
+            String firefoxProfileProperty = System.getProperty("webdriver.firefox.profile");
+            if (firefoxProfileProperty == null) {
+                ProfilesIni allProfiles = new ProfilesIni();
+                // Use the default profile to make extensions available,
+                // and especially to ease debugging with Firebug
+                firefoxProfile = allProfiles.getProfile("default");
+            }
+            return new LocalFirefox(firefoxProfile);
+        } else if (BrowserType.SAFARI.equalsIgnoreCase(browser)) {
+            return new LocalBrowser<SafariDriver>(SafariDriver.class);
+        } else if (BrowserType.CHROME.equalsIgnoreCase(browser)) {
+            return new LocalBrowser<ChromeDriver>(ChromeDriver.class);
+        } else if (BrowserType.IE.equalsIgnoreCase(browser)) {
+            return new LocalBrowser<InternetExplorerDriver>(InternetExplorerDriver.class);
+        } else {
+            throw new RuntimeException("Unknown browser value: " + browser);
+        }
+
+    }
+}

src/main/java/com/ariatemplates/seleniumjavarobot/IRobot.java
@@ -1,0 +1,34 @@
+/*
+ * Copyright 2014 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ariatemplates.seleniumjavarobot;
+
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+
+public interface IRobot {
+    public BufferedImage createScreenCapture(Rectangle screenRect);
+
+    public void keyPress(int keycode);
+
+    public void keyRelease(int keycode);
+
+    public void mouseMove(int x, int y);
+
+    public void mousePress(int buttons);
+
+    public void mouseRelease(int buttons);
+
+    public void mouseWheel(int wheelAmt);
+}

src/main/java/com/ariatemplates/seleniumjavarobot/IRobotizedWebDriverFactory.java
@@ -1,0 +1,19 @@
+/*
+ * Copyright 2014 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ariatemplates.seleniumjavarobot;
+
+public interface IRobotizedWebDriverFactory {
+    public RobotizedWebDriver createRobotizedWebDriver();
+}

src/main/java/com/ariatemplates/seleniumjavarobot/LocalRobot.java
@@ -1,0 +1,57 @@
+/*
+ * Copyright 2014 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ariatemplates.seleniumjavarobot;
+
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.image.BufferedImage;
+
+public class LocalRobot implements IRobot {
+
+    private Robot robot;
+
+    public LocalRobot(Robot robot) {
+        this.robot = robot;
+    }
+
+    public BufferedImage createScreenCapture(Rectangle screenRect) {
+        return robot.createScreenCapture(screenRect);
+    }
+
+    public void keyPress(int keycode) {
+        robot.keyPress(keycode);
+    }
+
+    public void keyRelease(int keycode) {
+        robot.keyRelease(keycode);
+    }
+
+    public void mouseMove(int x, int y) {
+        robot.mouseMove(x, y);
+    }
+
+    public void mousePress(int buttons) {
+        robot.mousePress(buttons);
+    }
+
+    public void mouseRelease(int buttons) {
+        robot.mouseRelease(buttons);
+    }
+
+    public void mouseWheel(int wheelAmt) {
+        robot.mouseWheel(wheelAmt);
+    }
+
+}

src/main/java/com/ariatemplates/seleniumjavarobot/Main.java
@@ -15,138 +15,78 @@
 
 package com.ariatemplates.seleniumjavarobot;
 
-import java.awt.Robot;
-import java.awt.Toolkit;
-import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.exec.OS;
-import org.openqa.selenium.Point;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxBinary;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.firefox.internal.ProfilesIni;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.safari.SafariDriver;
-
-import com.ariatemplates.seleniumjavarobot.calibrator.Calibrator;
-import com.ariatemplates.seleniumjavarobot.executor.Executor;
 
 public class Main {
     public final static List<String> BROWSERS_LIST = Arrays.asList(BrowserType.FIREFOX, BrowserType.SAFARI, BrowserType.CHROME, BrowserType.IE);
 
     public static void main(String[] args) throws Exception {
-        boolean autoRestart = false;
-        String browser = "";
+        SeleniumJavaRobot seleniumJavaRobot = new SeleniumJavaRobot();
+        String browser;
+        seleniumJavaRobot.autoRestart = false;
         if (OS.isFamilyMac()) {
             browser = "safari";
         } else {
             browser = "firefox";
         }
-        String url = "http://localhost:7777/__attester__/slave.html";
+        seleniumJavaRobot.url = "http://localhost:7777/__attester__/slave.html";
         String usageString = String
                 .format("Usage: selenium-java-robot [options]\nOptions:\n  --auto-restart\n  --url <url> [default: %s]\n  --browser <browser> [default: %s]\n\nAccepted browser values: %s",
-                        url, browser, BROWSERS_LIST.toString());
+                        seleniumJavaRobot.url, browser, BROWSERS_LIST.toString());
         for (int i = 0, l = args.length; i < l; i++) {
             String curParam = args[i];
             if ("--browser".equalsIgnoreCase(curParam) && i + 1 < l) {
                 browser = args[i + 1];
                 i++;
             } else if ("--url".equalsIgnoreCase(curParam) && i + 1 < l) {
-                url = args[i + 1];
+                seleniumJavaRobot.url = args[i + 1];
                 i++;
             } else if ("--auto-restart".equalsIgnoreCase(curParam)) {
-                autoRestart = true;
+                seleniumJavaRobot.autoRestart = true;
             } else if ("--help".equalsIgnoreCase(curParam)) {
                 System.out.println(usageString);
                 return;
             }
         }
-        ThreadPoolExecutor quitExecutor = new ThreadPoolExecutor(1, 1, 0, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>());
-        do {
-            final RemoteWebDriver driver = createWebDriver(browser);
-            try {
-                startDriver(driver, url);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            quitExecutor.execute(new Runnable() {
-                public void run() {
-                    // Makes sure the driver is closed. This is done
-                    // asynchronously so that we don't loose too much time
-                    // when --auto-restart is used, because the quit method
-                    // can take a long time to finish in case the browser
-                    // crashed or was terminated forcefully.
-                    try {
-                        driver.quit();
-                    } catch (Exception e) {
-                    }
+        seleniumJavaRobot.robotizedWebDriverFactory = LocalRobotizedWebDriverFactory.createRobotizedWebDriverFactory(browser);
+        seleniumJavaRobot.start();
+        closeOnStreamEnd(seleniumJavaRobot, System.in);
+        closeOnProcessEnd(seleniumJavaRobot);
+    }
+
+    private static void closeOnProcessEnd(final SeleniumJavaRobot seleniumJavaRobot) {
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            public void run() {
+                try {
+                    seleniumJavaRobot.stop();
+                } catch (InterruptedException e) {
                 }
-            });
-        } while (autoRestart);
-        quitExecutor.shutdown();
-        quitExecutor.awaitTermination(1, TimeUnit.MINUTES);
-        System.exit(0);
-    }
-
-    private static void numLockStateWorkaround() {
-        try {
-            // Shift key is not kept pressed while using keyPress method
-            // cf https://forums.oracle.com/thread/2232592
-            // The workaround is to use the following line:
-            System.out.println("Setting num lock state...");
-            Toolkit.getDefaultToolkit().setLockingKeyState(KeyEvent.VK_NUM_LOCK, false);
-        } catch (UnsupportedOperationException e) {
-            System.out.println("Setting num lock state is not supported: " + e);
-        }
-    }
-
-    public static void startDriver(RemoteWebDriver driver, String url) throws Exception {
-        Robot robot = new Robot();
-        Point offset = Calibrator.calibrate(driver, robot);
-        System.out.println("Computed offset: " + offset);
-        driver.get(url);
-        numLockStateWorkaround();
-        Executor.startExecutor(driver, robot, offset);
-    }
-
-    public static RemoteWebDriver createWebDriver(String browser) {
-        RemoteWebDriver driver;
-        if (BrowserType.FIREFOX.equalsIgnoreCase(browser)) {
-            FirefoxProfile firefoxProfile = null;
-            String firefoxProfileProperty = System.getProperty("webdriver.firefox.profile");
-            if (firefoxProfileProperty == null) {
-                ProfilesIni allProfiles = new ProfilesIni();
-                // Use the default profile to make extensions available,
-                // and especially to ease debugging with Firebug
-                firefoxProfile = allProfiles.getProfile("default");
             }
-            driver = new FirefoxDriver(firefoxProfile);
-        } else if (BrowserType.SAFARI.equalsIgnoreCase(browser)) {
-            driver = new SafariDriver();
-        } else if (BrowserType.CHROME.equalsIgnoreCase(browser)) {
-            driver = new ChromeDriver();
-        } else if (BrowserType.IE.equalsIgnoreCase(browser)) {
-            driver = new InternetExplorerDriver();
-        } else {
-            throw new RuntimeException("Unknown browser value: " + browser);
-        }
+        }));
+    }
 
-        if (OS.isFamilyMac()) {
-            try {
-                // put the browser in the foreground:
-                Runtime.getRuntime().exec("open -a " + browser);
-            } catch (Exception e) {
+    private static void closeOnStreamEnd(final SeleniumJavaRobot seleniumJavaRobot, final InputStream inputStream) {
+        Thread thread = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    while (inputStream.read() > -1) {
+                        // do nothing
+                    }
+                } catch (IOException e) {
+                }
+                try {
+                    seleniumJavaRobot.stop();
+                } catch (InterruptedException e) {
+                }
             }
-        }
-        driver.manage().window().maximize();
-        return driver;
+        });
+        thread.setDaemon(true);
+        thread.start();
     }
 }

src/main/java/com/ariatemplates/seleniumjavarobot/RobotizedWebDriver.java
@@ -1,0 +1,45 @@
+/*
+ * Copyright 2014 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ariatemplates.seleniumjavarobot;
+
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class RobotizedWebDriver {
+    private boolean stopped;
+    public final IRobot robot;
+    public final RemoteWebDriver webDriver;
+
+    public RobotizedWebDriver(IRobot robot, RemoteWebDriver webDriver) {
+        if (robot == null || webDriver == null) {
+            throw new NullPointerException();
+        }
+        this.robot = robot;
+        this.webDriver = webDriver;
+    }
+
+    public synchronized boolean isStopped() {
+        return stopped;
+    }
+
+    public synchronized void stop() {
+        if (!stopped) {
+            stopped = true;
+            try {
+                webDriver.quit();
+            } catch (RuntimeException e) {
+            }
+        }
+    }
+}

src/main/java/com/ariatemplates/seleniumjavarobot/SeleniumJavaRobot.java
@@ -1,0 +1,119 @@
+/*
+ * Copyright 2014 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ariatemplates.seleniumjavarobot;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Point;
+
+import com.ariatemplates.seleniumjavarobot.calibrator.Calibrator;
+import com.ariatemplates.seleniumjavarobot.executor.Executor;
+
+public class SeleniumJavaRobot {
+    // Public options (not supposed to be changed after calling start):
+    public String url;
+    public boolean autoRestart;
+    public IRobotizedWebDriverFactory robotizedWebDriverFactory;
+
+    // Private fields:
+    private final Thread mainThread = createMainThread();
+    private final ExecutorService quitExecutor = Executors.newSingleThreadExecutor();
+
+    private final Object lock = new Object();
+    // The previous lock object is a lock for the following 2 fields:
+    private RobotizedWebDriver robotizedWebDriver;
+    private boolean stopped = false;
+
+    public void start() {
+        mainThread.start();
+    }
+
+    public void stop() throws InterruptedException {
+        synchronized (lock) {
+            if (!stopped && mainThread.isAlive()) {
+                log("Closing ...");
+            }
+            stopped = true;
+            if (robotizedWebDriver != null) {
+                robotizedWebDriver.stop();
+            }
+        }
+        mainThread.join();
+    }
+
+    private Thread createMainThread() {
+        Thread result = new Thread(new Runnable() {
+            public void run() {
+                do {
+                    RobotizedWebDriver robotizedWebDriver = null;
+                    try {
+                        synchronized (lock) {
+                            if (stopped) {
+                                break;
+                            }
+                            robotizedWebDriver = robotizedWebDriverFactory.createRobotizedWebDriver();
+                            SeleniumJavaRobot.this.robotizedWebDriver = robotizedWebDriver;
+                        }
+                        startDriver(robotizedWebDriver, url);
+                    } catch (RuntimeException e) {
+                        e.printStackTrace();
+                    } catch (InterruptedException e) {
+                        break;
+                    } finally {
+                        if (robotizedWebDriver != null) {
+                            stopBrowserLater(robotizedWebDriver);
+                        }
+                    }
+                } while (autoRestart);
+                quitExecutor.shutdown();
+                try {
+                    quitExecutor.awaitTermination(1, TimeUnit.MINUTES);
+                } catch (InterruptedException e) {
+                }
+                SeleniumJavaRobot.log("End");
+            }
+        });
+        result.setDaemon(false);
+        return result;
+    }
+
+    private void stopBrowserLater(final RobotizedWebDriver robotizedWebDriver) {
+        quitExecutor.execute(new Runnable() {
+            public void run() {
+                // Makes sure the driver is closed. This is done
+                // asynchronously so that we don't loose too
+                // much time when --auto-restart is used,
+                // because the quit method can take a long time
+                // to finish in case the browser crashed or was
+                // terminated forcefully.
+                robotizedWebDriver.stop();
+            }
+        });
+    }
+
+    public static void startDriver(RobotizedWebDriver robotizedWebDriver, String url) throws InterruptedException {
+        Point offset = Calibrator.calibrate(robotizedWebDriver);
+        log("Computed offset: " + offset);
+        robotizedWebDriver.webDriver.get(url);
+        Executor executor = new Executor(robotizedWebDriver, offset);
+        executor.run();
+    }
+
+    public static void log(String log) {
+        System.out.println("[Selenium Java Robot] " + log);
+    }
+}

src/main/java/com/ariatemplates/seleniumjavarobot/calibrator/Calibrator.java
@@ -17,14 +17,16 @@ package com.ariatemplates.seleniumjavarobot.calibrator;
 
 import java.awt.Color;
 import java.awt.Rectangle;
-import java.awt.Robot;
 import java.io.IOException;
 import java.util.Map;
 
 import org.apache.commons.io.IOUtils;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.WebDriver.Window;
+
+import com.ariatemplates.seleniumjavarobot.RobotizedWebDriver;
+import com.ariatemplates.seleniumjavarobot.SeleniumJavaRobot;
 
 public class Calibrator {
 
@@ -41,25 +43,27 @@ public class Calibrator {
     private static final int DEFAULT_COLOR_TOLERANCE = 50;
     private static final int BORDER = 10;
 
-    public static Point calibrate(RemoteWebDriver driver, Robot robot) throws InterruptedException {
-        return calibrate(driver, robot, DEFAULT_CALIBRATION_COLOR, DEFAULT_COLOR_TOLERANCE);
+    public static Point calibrate(RobotizedWebDriver driver) throws InterruptedException {
+        return calibrate(driver, DEFAULT_CALIBRATION_COLOR, DEFAULT_COLOR_TOLERANCE);
     }
 
-    public static Point calibrate(RemoteWebDriver driver, Robot robot, Color calibrationColor, int colorTolerance) throws InterruptedException {
+    public static Point calibrate(RobotizedWebDriver robotizedWebDriver, Color calibrationColor, int colorTolerance) throws InterruptedException {
         // call the calibration script:
-        Map<String, Long> jsInfos = (Map<String, Long>) driver.executeScript(CALIBRATOR_SCRIPT,
+        Map<String, Long> jsInfos = (Map<String, Long>) robotizedWebDriver.webDriver.executeScript(CALIBRATOR_SCRIPT,
                 String.format("rgb(%d,%d,%d)", calibrationColor.getRed(), calibrationColor.getGreen(), calibrationColor.getBlue()), BORDER);
         int width = jsInfos.get("width").intValue();
         int height = jsInfos.get("height").intValue();
-        System.out.println(String.format("Viewport size: %d x %d", width, height));
-        Point windowPosition = driver.manage().window().getPosition();
-        Dimension windowSize = driver.manage().window().getSize();
+        SeleniumJavaRobot.log(String.format("Viewport size: %d x %d", width, height));
+        Window window = robotizedWebDriver.webDriver.manage().window();
+        Point windowPosition = window.getPosition();
+        Dimension windowSize = window.getSize();
         Rectangle windowRectangle = new Rectangle(windowPosition.x, windowPosition.y, windowSize.width, windowSize.height);
-        System.out.println("Browser window rectangle: " + windowRectangle);
+        SeleniumJavaRobot.log("Browser window rectangle: " + windowRectangle);
         // Give some time to the browser to display the expected color:
-        Thread.currentThread().sleep(200);
+        Thread.sleep(200);
         // look for the rectangle full of the expected color:
-        Rectangle rect = RectangleFinder.findRectangle(robot, calibrationColor, windowRectangle, width - 2 * BORDER, height - 2 * BORDER, colorTolerance);
+        Rectangle rect = RectangleFinder.findRectangle(robotizedWebDriver.robot, calibrationColor, windowRectangle, width - 2 * BORDER, height - 2 * BORDER,
+                colorTolerance);
         if (rect == null) {
             throw new RuntimeException("Calibration failed.");
         }

commit 6ef1b751cb1529479fc9af6f26c1b8b27161dd58

Fixing a regression when putting Safari in the foreground
The regression was introduced in 5f2462db6e57cf3a5ab0d3fc2c477acb1eb1d0db.
Using quotes prevents the command from being successful when using
Runtime.getRuntime.exec():
Not working: open -a "safari"
Working fine: open -a safari

src/main/java/com/ariatemplates/seleniumjavarobot/LocalRobotizedWebDriverFactory.java
@@ -59,13 +59,6 @@ public abstract class LocalRobotizedWebDriverFactory implements IRobotizedWebDri
 
     public RobotizedWebDriver createRobotizedWebDriver() {
         RemoteWebDriver driver = createWebDriver();
-        if (OS.isFamilyMac()) {
-            try {
-                // put the browser in the foreground:
-                Runtime.getRuntime().exec("open -a \"" + driver.getCapabilities().getBrowserName() + "\"");
-            } catch (Exception e) {
-            }
-        }
         driver.manage().window().maximize();
         return new RobotizedWebDriver(robot, driver);
     }
@@ -104,6 +97,23 @@ public abstract class LocalRobotizedWebDriverFactory implements IRobotizedWebDri
         }
     }
 
+    public static class LocalSafari extends LocalRobotizedWebDriverFactory {
+        @Override
+        public RemoteWebDriver createWebDriver() {
+            SafariDriver safari = new SafariDriver();
+            if (OS.isFamilyMac()) {
+                try {
+                    // put the browser in the foreground:
+                    String cmdline = "open -a safari";
+                    SeleniumJavaRobot.log("Executing: " + cmdline);
+                    Runtime.getRuntime().exec(cmdline);
+                } catch (Exception e) {
+                }
+            }
+            return safari;
+        }
+    }
+
     public static LocalRobotizedWebDriverFactory createRobotizedWebDriverFactory(String browser) {
         if (BrowserType.FIREFOX.equalsIgnoreCase(browser)) {
             FirefoxProfile firefoxProfile = null;
@@ -116,7 +126,7 @@ public abstract class LocalRobotizedWebDriverFactory implements IRobotizedWebDri
             }
             return new LocalFirefox(firefoxProfile);
         } else if (BrowserType.SAFARI.equalsIgnoreCase(browser)) {
-            return new LocalBrowser<SafariDriver>(SafariDriver.class);
+            return new LocalSafari();
         } else if (BrowserType.CHROME.equalsIgnoreCase(browser)) {
             return new LocalBrowser<ChromeDriver>(ChromeDriver.class);
         } else if (BrowserType.IE.equalsIgnoreCase(browser)) {